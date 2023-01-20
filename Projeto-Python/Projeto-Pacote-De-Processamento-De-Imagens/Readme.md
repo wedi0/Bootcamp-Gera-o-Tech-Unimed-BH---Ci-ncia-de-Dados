b047d248b619b7de177d68fa0423b212ed130a98
Weslley <wediweslley@gmail.com>
2023-01-20 19:07:41 -0300

    commit final

--- Projeto-Python/Projeto-Pacote-De-Processamento-De-Imagens/Readme.md
+++ Projeto-Python/Projeto-Pacote-De-Processamento-De-Imagens/Readme.md
@@ -1,1 +1,43 @@
-Projeto em desenvolvimento...
+# Pacote de processamento de imagens
+
+
+
+## **Descriçao**:
+O pacote image_processing ele é usado para:
+
+**Módulo Processing**:
+
+* Correspondência de histograma;
+* Similaridade estrutural;
+* Redimensionar imagem;
+
+**Módulo "Utils"**:
+
+* Ler imagem;
+* Salvar imagem;
+* Plotar imagem;
+* Resultado do gráfico;
+* Plotar histograma;
+
+-----------------------------------------
+## *Preparando o ambiente para Test Pypi*
+
+Comandos de instalação:
+
+```
+- python -m pip install --upgrade pip
+- python -m pip install --user twine
+- python -m pip install --user setuptools
+```
+Comandos para criar distribuições:
+
+````
+- python setup.py sdist bdist_wheel
+````
+
+ Após completar a instalação, verifique se as pastas abaixo foram adicionadas ao projeto:
+  *  build;
+  *  dist;
+  *  image_processing_test.egg-info.
+
+Basta subir os arquivos, usando o Twine, para o Test Pypi:
--- Projeto-Python/Projeto-Pacote-De-Processamento-De-Imagens/setup.py
+++ Projeto-Python/Projeto-Pacote-De-Processamento-De-Imagens/setup.py
@@ -7,13 +7,14 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name = "image_processing-teste",
+    name = "image_processing-wedi",
     version = "0.0.3",
     author = "Weslley Augusto de Oliviera",
+    author_email="wedi01@hotmail.com",
     description="Test version - Image processing. This project belongs to Karina Tiemi Kato, Tech Lead, Machine Learning Engineer, Data Scientist Specialist at Take. This package is a demo for simulation of upload on the Test Pypi website, and it's from class of the Bootcamp developer full stack Python. E-mail:karinatkato@gmail.com.",
     long_description = page_description,
     long_description_content_type = "text/markdown",
-    url = "",
+    url = "https://github.com/wedi0/Bootcamp-Geracao-Tech-Unimed-BH--Ciencia-de-Dados/tree/main/Projeto-Python/Projeto-Pacote-De-Processamento-De-Imagens",
     packages= find_packages(),
     install_requires = requirements,
     python_requires = '>=3.8',
